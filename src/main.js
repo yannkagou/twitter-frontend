07fc6b74f918628964639caea117ac5d5e315684
unknown <yannickkagou@gmail.com>
2024-01-31 20:31:56 +0100

    Add the axios package

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import '@fortawesome/fontawesome-free/css/all.css'
 import './assets/main.css'
 import 'material-icons/iconfont/material-icons.css'
+import axios from 'axios'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -10,7 +11,10 @@ import router from './router'
 
 const app = createApp(App)
 
+
+axios.defaults.baseURL = 'http://localhost:8000'
+
 app.use(createPinia())
-app.use(router)
+app.use(router, axios)
 
 app.mount('#app')
